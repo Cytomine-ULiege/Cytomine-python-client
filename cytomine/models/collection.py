0bad4ca73e6e02816d9cb6c379bbfd41acb8732e
Ba Thien LE <bathien.le.pro@gmail.com>
2021-11-09 17:15:15 +0100

    Add docs for Collection

--- cytomine/models/collection.py
+++ cytomine/models/collection.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# * Copyright (c) 2009-2018. Authors: see NOTICE file.
+# * Copyright (c) 2009-2021. Authors: see NOTICE file.
 # *
 # * Licensed under the Apache License, Version 2.0 (the "License");
 # * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ from __future__ import unicode_literals
 
 __author__ = "Rubens Ulysse <urubens@uliege.be>"
 __contributors__ = ["Marée Raphaël <raphael.maree@uliege.be>", "Mormont Romain <r.mormont@uliege.be>"]
-__copyright__ = "Copyright 2010-2018 University of Liège, Belgium, http://www.cytomine.be/"
+__copyright__ = "Copyright 2010-2021 University of Liège, Belgium, http://www.cytomine.be/"
 
 from collections import MutableSequence
 
@@ -34,16 +34,19 @@ from ._utilities.parallel import generic_chunk_parallel
 
 class CollectionPartialUploadException(Exception):
     """To be thrown when a collection is saved but only a part of it was successfully done so."""
+
     def __init__(self, desc, created=None, failed=None):
         """
+        Initialize the exception.
+
         Parameters
         ----------
-        desc: str
-            Description of the exception
-        created: Collection
+        desc : str
+            Description of the exception.
+        created : Collection, default=None
             A Cytomine collection (same type as the one saved) containing the successfully saved objects (with their
             created ids).
-        failed: Collection
+        failed : Collection, default=None
             A Cytomine collection (same type as the one saved) containing the objects that couldn't be saved.
         """
         super(CollectionPartialUploadException, self).__init__(desc)
@@ -52,15 +55,49 @@ class CollectionPartialUploadException(Exception):
 
     @property
     def created(self):
+        """
+        Get the part of the collection that was successfully saved.
+
+        Returns
+        -------
+        created : Collection
+            The part of the collection that was saved.
+        """
         return self._created
 
     @property
     def failed(self):
+        """
+        Get the part of the collection that couldn't be saved.
+
+        Returns
+        -------
+        failed : Collection
+            The part of the collection that couldn't be saved.
+        """
         return self._failed
 
 
 class Collection(MutableSequence):
+    """
+    A collection object.
+    """
+
     def __init__(self, model, filters=None, max=0, offset=0):
+        """
+        Initialize a collection instance.
+
+        Parameters
+        ----------
+        model : Model
+            A model.
+        filters : dict, default=None
+            The filters to apply on this collection.
+        max : int, default=0
+            The number of maximum items to fetch in one request.
+        offset : int, default=0
+            The offset for the next page.
+        """
         self._model = model
         self._data = []
 
@@ -82,14 +119,16 @@ class Collection(MutableSequence):
     def fetch(self, max=None):
         """
         Fetch all collection by pages of `max` items.
+
         Parameters
         ----------
-        max : int, None (optional)
-            The number of item per page. If None, retrieve all collection.
+        max : int, default=None
+            The number of items per page. If None, retrieve all collection.
 
         Returns
         -------
-        self    Collection, the fetched collection
+        self : Collection
+            The fetched collection.
         """
         if max:
             self.max = max
@@ -103,14 +142,52 @@ class Collection(MutableSequence):
             return self._fetch()
 
     def fetch_with_filter(self, key, value, max=None):
+        """
+        Fetch a collection with the given filter.
+
+        Parameters
+        ----------
+        key : str
+            The key of the filter.
+        value : str
+            The value of the filter.
+        max : int, default=None
+            The maximum number of items to fetch. If None, retrieve all collection.
+
+        Returns
+        -------
+        self : Collection
+            The fetched collection.
+        """
         self._filters[key] = value
         return self.fetch(max)
 
     def fetch_next_page(self, append_mode=False):
+        """
+        Fetch the collection on the next page.
+
+        Parameters
+        ----------
+        append_mode : bool, default=False
+            Whether to append the collection to the current data or not.
+
+        Returns
+        -------
+        self : Collection
+            The fetched collection.
+        """
         self.offset = min(self._total, self.offset + self.max)
         return self._fetch(append_mode)
 
     def fetch_previous_page(self):
+        """
+        Fetch the collection on the previous page.
+
+        Returns
+        -------
+        self : Collection
+            The fetched collection.
+        """
         self.offset = max(0, self.offset - self.max)
         return self._fetch()
 
@@ -123,10 +200,27 @@ class Collection(MutableSequence):
 
     def save(self, chunk=15, n_workers=0):
         """
-        chunk: int|None
-             Maximum number of object to send at once in a single HTTP request. None for sending them all at once.
-        n_workers: int
-            Number of threads to use for sending chunked requests (ignored if chunk is None). Value 0 for using as many threads as cpus on the machine.
+        Save the collection.
+
+        Parameters
+        ----------
+        chunk : int, default=15
+            Maximum number of object to send at once in a single HTTP request. None for sending them all at once.
+        n_workers : int, default=0
+            Number of threads to use for sending chunked requests (ignored if chunk is None).
+            Value 0 for using as many threads as cpus on the machine.
+
+        Returns
+        -------
+        response : bool
+            The True if the collection was successfully saved, False otherwise.
+
+        Raises
+        ------
+        CollectionPartialUploadException
+            If the entire collection failed to be saved.
+        ValueError
+            If there is an invalid value for a chunk parameter.
         """
         if chunk is None:
             return Cytomine.get_instance().post_collection(self)
@@ -143,9 +237,37 @@ class Collection(MutableSequence):
             raise ValueError("Invalid value '{}' for chunk parameter.".format(chunk))
 
     def to_json(self, **dump_parameters):
+        """
+        Convert the parameters to a JSON format.
+
+        Parameters
+        ----------
+        dump_parameters : dict
+            The parameters to convert to.
+
+        Returns
+        -------
+        parameters : str
+            The parameters in JSON format.
+        """
         return "[{}]".format(",".join([d.to_json(**dump_parameters) for d in self._data]))
 
     def populate(self, attributes, append_mode=False):
+        """
+        Populate the collection with attributes.
+
+        Parameters
+        ----------
+        attributes : dict
+            The attributes to add.
+        append_mode : bool, default=False
+            Whether to append the attributes or overwrite the existing ones.
+
+        Returns
+        -------
+        self : Collection
+            The populated collection.
+        """
         data = [self._model().populate(instance) for instance in attributes["collection"]]
         if append_mode:
             self._data += data
@@ -160,15 +282,59 @@ class Collection(MutableSequence):
 
     @property
     def filters(self):
+        """
+        Get the filters applied on this collection.
+
+        Returns
+        -------
+        filters : dict
+            The filters.
+        """
         return self._filters
 
     def is_filtered_by(self, key):
+        """
+        Check whether a key is present in the filters or not.
+
+        Parameters
+        ----------
+        key : str
+            The key to check.
+
+        Returns
+        -------
+        response : bool
+            True if the key is in the filters, False otherwise.
+        """
         return key in self._filters
 
     def add_filter(self, key, value):
+        """
+        Add a new filter on the collection.
+
+        Parameters
+        ----------
+        key : str
+            The key of the filter.
+        value : str
+            The value of the filter.
+        """
         self._filters[key] = value
 
     def set_parameters(self, parameters):
+        """
+        Set the parameters.
+
+        Parameters
+        ----------
+        parameters : dict
+            The parameters to add.
+
+        Returns
+        -------
+        self : Collection
+            The collection with the parameters.
+        """
         if parameters:
             for key, value in six.iteritems(parameters):
                 if not key.startswith("_"):
@@ -177,6 +343,14 @@ class Collection(MutableSequence):
 
     @property
     def parameters(self):
+        """
+        Get the parameters of this collection.
+
+        Returns
+        -------
+        params : dict
+            The parameters.
+        """
         params = dict()
         for k, v in six.iteritems(self.__dict__):
             if v is not None and not k.startswith("_"):
@@ -187,9 +361,30 @@ class Collection(MutableSequence):
 
     @property
     def callback_identifier(self):
+        """
+        Get the callback identifier.
+
+        Returns
+        -------
+        name : str
+            The callback identifier.
+        """
         return self._model.__name__.lower()
 
     def uri(self, without_filters=False):
+        """
+        Get the URI of this collection.
+
+        Parameters
+        ----------
+        without_filters : bool, default=False
+            Whether to get the URI with filters or not.
+
+        Returns
+        -------
+        uri : str
+            The URI.
+        """
         uri = ""
         if not without_filters:
             if len(self.filters) > 1:
@@ -203,17 +398,19 @@ class Collection(MutableSequence):
         return "{}{}.json".format(uri, self.callback_identifier)
 
     def find_by_attribute(self, attr, value):
-        """Retrieve the first item of which the item.attr matches 'value'
+        """
+        Retrieve the first item of which the item.attr matches 'value'.
+
         Parameters
         ----------
-        attr: str
-            Name of the attribute
-        value: str
-            The value to find
+        attr : str
+            Name of the attribute.
+        value : str
+            The value to find.
 
         Returns
         -------
-        item: object|None
+        item : object or None
             The object retrieved from the list, or None if not found.
         """
         return next(iter([i for i in self if hasattr(i, attr) and getattr(i, attr) == value]), None)
@@ -259,6 +456,14 @@ class Collection(MutableSequence):
         return collection
 
     def data(self):
+        """
+        Get the data of this collection.
+
+        Returns
+        -------
+        data : list
+            The data.
+        """
         return self._data
 
     def filter(self, fn):
